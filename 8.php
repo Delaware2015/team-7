0eb3be05a8d16fa67fdf7b99aad3c14890ec09fd
Ubuntu <ubuntu@ip-10-170-35-72.ec2.internal>
2015-10-31 14:31:28 +0000

    something

--- 8.php
+++ 8.php
@@ -15,20 +15,21 @@
 			
 								
 <?php
-require("database.php");
-$sql = "SELECT distinct t.deadline FROM tasks as t, student as s WHERE s.student_id = t.student_id ORDER BY t.deadline";
+/*require("database.php");
+$sql = "SLECT distinct t.deadline FROM tasks as t, student as s WHERE s.student_id = t.student_id ORDER BY t.deadline";
 $result = $conn->query($sql);
 
 while($row = $result->fetch_assoc()) {
-	
+	*/
 ?>
 			<div class="row">
 				<div class="col-xs-12">
-					<h3><i><?php echo $row['deadline']; ?></i></h3>
+					<h3><i><?//php echo $row['deadline']; ?></i></h3>
 				</div>
 			</div>
 
 			<table class="table-condensed">
+
 			</table>
 		</div>
 	<body>
