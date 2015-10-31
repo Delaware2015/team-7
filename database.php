1eb439a6ec9fb5cbb7554dcd74fabbdb5393654f
julierogers <juanroge@indiana.edu>
2015-10-30 23:31:31 -0400

    database file

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "http://ec2-54-87-67-182.compute-1.amazonaws.com";
+$username = "root";
+$password = "code4good";
+$dbname = "code4good.db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+?>
