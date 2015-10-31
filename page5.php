374b8536db3ff450e536cfbb707051ce77c918d6
youngadj <youngadj@indiana.edu>
2015-10-31 00:46:29 -0400

    Update page5.php

--- page5.php
+++ page5.php
@@ -1,14 +1,39 @@
-<?php
+<html>
+	<head>
+		<title>Search Colleges</title>
 
-require("database.php");
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="row">
+			<div class="col-xs-offset-1 col-xs-10">
+				<div class="input-group">
+				  <input type="text" class="form-control" placeholder="Search for Colleges" aria-describedby="basic-addon2">
+				  <span class="input-group-addon glyphicon glyphicon-search" id="basic-addon2"></span>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<table>
+		<?php
 
-$sql = "SELECT * FROM school";
-$result = $conn->query($sql);
+		require("database.php");
 
-while($row = $result->fetch_assoc()) {
-	// This section will be where all of the college information goes.
-	// College id number, College name, college application deadline.
-	echo 'Name: ' . $row['school_name'] . " Enrollment Date: " . $row['enrollment_deadline'] . "<br>";
-}
-
-?>
+		$sql = "SELECT * FROM school";
+		$result = $conn->query($sql);
+		
+		echo '<th>Name</th><th>Enrollment Date</th>';
+		
+		while($row = $result->fetch_assoc()) {
+			// This section will be where all of the college information goes.
+			// College id number, College name, college application deadline.
+			echo '<tr><td>'.$row['school_name'].'</td><td>'.$row['enrollment_date'].'</td></tr>'
+		}
+		
+		?>
+			</table>
+		</div>
+	</body>
+</html>
