ea351671faab530dee3be7365336ef036fc6f7dd
juanroge15 <juanroge@indiana.edu>
2015-10-31 09:42:01 -0400

    Update page1.php
    added css style sheet

--- page1.php
+++ page1.php
@@ -5,6 +5,7 @@
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 </head>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
 <body>
 	
 <?php
