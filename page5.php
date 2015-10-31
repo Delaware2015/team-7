286f16b88e7e9c5e251bcac6c6d809a89391a56e
youngadj <youngadj@indiana.edu>
2015-10-31 00:35:17 -0400

    Update page5.php

--- page5.php
+++ page5.php
@@ -8,7 +8,7 @@ $result = $conn->query($sql);
 while($row = $result->fetch_assoc()) {
 	// This section will be where all of the college information goes.
 	// College id number, College name, college application deadline.
-	echo 'Name: ' . $row['school_name'] . " Enrollment Date: " . $row['enrollment_deadline'];
+	echo 'Name: ' . $row['school_name'] . " Enrollment Date: " . $row['enrollment_deadline'] . "<br>";
 }
 
 ?>
