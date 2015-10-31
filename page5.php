3f24e155faaaa09fffd201089b9705ada10c2906
youngadj <youngadj@indiana.edu>
2015-10-31 00:14:31 -0400

    Update page5.php

--- page5.php
+++ page5.php
@@ -1,6 +1,15 @@
 <?php
 
-require("database.php");
+$servername = "ec2-54-87-67-182.compute-1.amazonaws.com";
+$username = "root";
+$password = "code4good";
+$dbname = "code4good.db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error) {
+	die("connection failed " . $conn->connect_error);
+}
 
 $sql = "SELECT * FROM school";
 $result = $conn->query($sql);
