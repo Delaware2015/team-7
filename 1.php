0feb11f5156f9ea634829c43753d9c3849ea884c
youngadj <youngadj@indiana.edu>
2015-10-31 05:32:16 -0400

    Update 1.php

--- 1.php
+++ 1.php
@@ -9,7 +9,6 @@
 	
 <?php
 if (isset($_POST['submit'])){
-
 	$username = $_POST['inputEmail3'];
 	$password = $_POST['inputPassword3'];
 	echo $username;
@@ -26,14 +25,14 @@ if (isset($_POST['submit'])){
 			<div class="form-group">
 				   <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 				    <div class="col-sm-10 col-xs-12">
-				      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+				      <input type="email" class="form-control" name="inputEmail3" placeholder="Email">
 				    </div>
 			</div>
 
 			<div class="form-group">
 			  	<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
 			  	<div class="col-sm-10 col-xs-12">
-			      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+			      <input type="password" class="form-control" name="inputPassword3" placeholder="Password">
 			    </div>
 			</div>
 
