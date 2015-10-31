c5e876b6a39eb88427cb6665aecf40455f8b264b
youngadj <youngadj@indiana.edu>
2015-10-31 00:18:18 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,14 +1,15 @@
 <?php
 
-$servername = "ec2-54-87-67-182.compute-1.amazonaws.com";
+$servername = "localhost";
 $username = "root";
 $password = "code4good";
 $dbname = "code4good.db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+if($conn->connect_error) {
+	die("connection failed " . $conn->connect_error);
 }
+
 
 ?>
