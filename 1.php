d3a2d7915be3a98a9ee36fdc7b96dc63c7c4b233
youngadj <youngadj@indiana.edu>
2015-10-31 05:08:26 -0400

    Update 1.php

--- 1.php
+++ 1.php
@@ -13,8 +13,8 @@ if (isset($_POST['submit'])){
 	$username = $_POST['inputEmail3'];
 	$password = $_POST['inputPassword3'];
 	
-	if ($username == 'rberry95@yahoo.com') == 0 {
-		if ($password == 'team07') == 0 {
+	if ($username == 'rberry95@yahoo.com') {
+		if ($password == 'team07') {
 			header('Location: http://ec2-54-87-67-182.compute-1.amazonaws.com/team-7/2.html');
 		}
 	} else {
